ba2c42eca486c8471195508aac8594d620fadde1
ejim11 <favourejim56@gmail.com>
2024-12-27 23:21:28 +0100

    🎨 changed factory, pm and ep addresses

--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,23 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
 
-const EP_ADDR = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const EP_ADDR = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
-const ACC_ADDR = "0x75537828f2ce51be7289709686A69CbFDbB714F1";
+const ACC_ADDR = '0xCafac3dD18aC6c6e92c921884f9E4176737C052c';
+const PM_ADDR = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
 
 async function main() {
   // const code = await hre.ethers.provider.getCode(EP_ADDR);
 
   // console.log(code);
 
-  const smartAcc = await hre.ethers.getContractAt("Account", ACC_ADDR);
+  const smartAcc = await hre.ethers.getContractAt('Account', ACC_ADDR);
+  const ep = await hre.ethers.getContractAt('EntryPoint', EP_ADDR);
   const count = await smartAcc.count();
   console.log(count);
+
+  console.log('acc balance: ', await hre.ethers.provider.getBalance(ACC_ADDR));
+  console.log('acc Ep balance: ', await ep.balanceOf(ACC_ADDR));
+  console.log('acc Ep balance: ', await ep.balanceOf(PM_ADDR));
 }
 
 main().catch((error) => {
