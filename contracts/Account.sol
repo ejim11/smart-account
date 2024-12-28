6de07c328677f6208a06fb30b01eccd3fee3b0f4
ejim11 <favourejim56@gmail.com>
2024-12-28 16:43:04 +0100

    ✅ added function to validate userOp

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.28;
 
 import "@account-abstraction/contracts/core/EntryPoint.sol";
 import "@account-abstraction/contracts/interfaces/IAccount.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "hardhat/console.sol";
+
+// contract Test {
+//     constructor(bytes memory sig) {
+//         address recovered = ECDSA.recover(ECDSA.toEthSignedMessageHash(keccak256("wee")), sig);
+//         console.log(recovered);
+//     }
+// }
 
 contract Account is IAccount{
 uint public count;
@@ -13,13 +22,16 @@ constructor (address _owner) {
 }
 
     function validateUserOp(
-        UserOperation calldata ,
-        bytes32 ,
+        UserOperation calldata userOp ,
+        bytes32 userOpHash,
         uint256 
-    ) external  pure returns (uint256 validationData){
+    ) external  view returns (uint256 validationData){
         // allow any account
         // validation
-        return 0;
+        address recovered = ECDSA.recover(ECDSA.toEthSignedMessageHash(userOpHash), userOp.signature);
+         console.log(recovered, owner);
+        return owner == recovered ? 0: 1;
+        // return 0;
     }
 
     function execute() external {
