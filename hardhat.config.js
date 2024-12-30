966007e4481e7407046c6625c35439d4f4b4aa99
ejim11 <favourejim56@gmail.com>
2024-12-30 19:45:11 +0100

    ✅ changed the default network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: 'localhost',
+  defaultNetwork: 'arb',
   networks: {
     arb: {
       url: process.env.RPC_URL,
