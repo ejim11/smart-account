17d6abd313104e78c73fb5899abdbcfd605194d7
ejim11 <favourejim56@gmail.com>
2024-12-30 19:53:42 +0100

    ✅ updated tests

--- scripts/test.js
+++ scripts/test.js
@@ -2,7 +2,7 @@ const hre = require('hardhat');
 
 const EP_ADDR = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
-const ACC_ADDR = '0xCafac3dD18aC6c6e92c921884f9E4176737C052c';
+const ACC_ADDR = '0x6717261c38c10e119efa2e446869f9e025154ef8';
 const PM_ADDR = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
 
 async function main() {
