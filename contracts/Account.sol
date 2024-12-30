35a69a7ea71c932cde80c9d7f354d675c81c11ea
ejim11 <favourejim56@gmail.com>
2024-12-30 19:45:53 +0100

    🎨 changed to CREATE2 OPPCODE for using the bundler

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 import "@account-abstraction/contracts/core/EntryPoint.sol";
 import "@account-abstraction/contracts/interfaces/IAccount.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/Create2.sol";
 import "hardhat/console.sol";
 
 // contract Test {
@@ -41,7 +42,21 @@ constructor (address _owner) {
 
 contract AccountFactory {
     function createAccount(address owner) external returns(address) {
-        Account acc = new Account(owner);
-        return address(acc);
+        // amount, salt, bytecode,
+        bytes32 salt = bytes32(uint256(uint160(owner)));
+
+// creation code + comstructor arg
+        bytes memory bytecode = abi.encodePacked(type(Account).creationCode,abi.encode(owner));
+
+        address addr = Create2.computeAddress(salt, keccak256(bytecode));
+
+        if(addr.code.length > 0) {
+            return addr;
+        }
+
+        return Create2.deploy(0, salt , bytecode);
+
+        // Account acc = new Account(owner);
+        // return address(acc);
     }
 }
